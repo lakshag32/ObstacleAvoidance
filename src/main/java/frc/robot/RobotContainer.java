b362a830e08a7ea7f0c4ba4c7b96353e55137035
lakshag32 <lakshag32@gmail.com>
2024-03-22 19:08:37 -0700

    Delete files from 972's FRC 2024 repo to create a clean template for the following code

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,27 @@
+package frc.robot;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+
+  // The robot's subsystems are defined here...
+  private Drivetrain drive = null;
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   * <p>
+   * Different robots may have different subsystems.
+   */
+  public RobotContainer(RobotId robotId) {
+   
+  }
+}
+
+
